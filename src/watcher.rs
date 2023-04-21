cb9b9332f045596395361c320acb13d5b92f6cfe
zian <mynameiszian@gmail.com>
2023-04-21 12:45:55 +0800

    added start stop

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,22 +1,32 @@
+use std::path::Path;
 
+use notify::{Event, ReadDirectoryChangesWatcher, RecursiveMode, Watcher};
 
-use notify::{Event, ReadDirectoryChangesWatcher};
-
-pub struct Watcher {
+pub struct PluginDirectoryWatcher {
     dir: String,
-    _self: ReadDirectoryChangesWatcher,
+    inner: ReadDirectoryChangesWatcher,
 }
 
-impl Watcher {
+impl PluginDirectoryWatcher {
     pub fn new(dir: String) -> Self {
         let _self = notify::recommended_watcher(Self::handle_event).unwrap();
 
-        Self { dir, _self }
+        Self { dir, inner: _self }
     }
 
     fn handle_event(_event: Result<Event, notify::Error>) {}
 
-    pub fn start(&mut self) {}
+    pub fn start(&mut self) -> Result<bool, Box<dyn std::error::Error>> {
+        let mut watcher = self
+            .inner
+            .watch(Path::new(&self.dir), RecursiveMode::Recursive)?;
 
-    pub fn stop(&mut self) {}
+        Ok(true)
+    }
+
+    pub fn stop(&mut self) -> Result<bool, Box<dyn std::error::Error>> {
+        self.inner.unwatch(Path::new(&self.dir))?;
+
+        Ok(true)
+    }
 }
