cf5fa2ab1684d59dbfacaa412b07d2b509bf4ba7
zian <mynameiszian@gmail.com>
2023-04-21 12:05:23 +0800

    add watcher skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
+mod watcher;
+
 use std::path::Path;
 
 use libloading::Symbol;
 use notify::{RecommendedWatcher, RecursiveMode, Result, Watcher};
 
 fn main() -> Result<()> {
-    // // Automatically select the best implementation for your platform.
-    // let mut watcher = notify::recommended_watcher(|res| {
-    //     match res {
-    //        Ok(event) => println!("event: {:?}", event),
-    //        Err(e) => println!("watch error: {:?}", e),
-    //     }
-    // })?;
+    // Automatically select the best implementation for your platform.
+    let mut watcher = notify::recommended_watcher(|res| {
+        match res {
+           Ok(event) => println!("event: {:?}", event),
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    })?;
 
-    // // Add a path to be watched. All files and directories at that path and
-    // // below will be monitored for changes.
-    // watcher.watch(Path::new("./src/plugins"), RecursiveMode::Recursive)?;
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new("./src/plugins"), RecursiveMode::Recursive)?;
 
-    // loop {
+    loop {
 
-    // }
+    }
 
     let compile = std::process::Command::new("rustc")
         .arg("--crate-type")
---
+++ src/watcher.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+
+use notify::{Event, ReadDirectoryChangesWatcher};
+
+pub struct Watcher {
+    dir: String,
+    _self: ReadDirectoryChangesWatcher,
+}
+
+impl Watcher {
+    pub fn new(dir: String) -> Self {
+        let _self = notify::recommended_watcher(Self::handle_event).unwrap();
+
+        Self { dir, _self }
+    }
+
+    fn handle_event(event: Result<Event, notify::Error>) {}
+
+    pub fn start(&mut self) {}
+
+    pub fn stop(&mut self) {}
+}
